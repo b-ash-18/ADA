20fb423b9e4b750db919cfecb0371a10fa42f209
Ashish B <115473995+b-ash-18@users.noreply.github.com>
2023-08-08 22:06:55 +0530

    added for min for second loop lesser runs

--- binomial.cpp
+++ binomial.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
 
+int min(int a, int b){
+    return (a < b) ? a : b ;
+}
+
 // int binomial(int n, int r){
 //     int matrix[n+1][r+1];
-//     for(int i = 0; i < n+1; i++){
-//         for(int j = 0; j < r+1; j++){
+//     for(int i = 0; i <= n; i++){
+//         for(int j = 0; j <= min(i,r); j++){
 //             if(j == 0 || i == j){
 //                 matrix [i][j] = 1;
 //             }
